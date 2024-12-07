08585b4e543833c1d9c3b925ec603a0452e2b4ef
Kushal Harsora <kushalharsora13@gmail.com>
2024-12-07 18:59:56 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <main className=' h-screen w-screen flex justify-center items-center text-4xl font-mono underline decoration-wavy decoration-slate-400'>
+        Hello World
+      </main>
+    </React.Fragment>
+  )
+}
+
+export default App
